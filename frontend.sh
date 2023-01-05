0ccaafb06c542f6b7e0707192ff30e231146488a
Sudarsana K <sudak80@gmail.com>
2023-01-05 05:48:03 +0530

    updated frontend details

--- frontend.sh
+++ frontend.sh
@@ -1,1 +1,19 @@
 # This script is to automate frontend configuration
+current_location=$(pwd)
+exit
+
+yum install nginx -y
+
+systemctl enable nginx 
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/* 
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+
+cp $(current_location)/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx 
