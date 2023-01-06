1803ff50669eac18b296fc3ec5e90a715cace5d4
Sudarsana K <sudak80@gmail.com>
2023-01-06 06:40:04 +0530

    conffile_location added

--- frontend.sh
+++ frontend.sh
@@ -8,10 +8,10 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-#cd /usr/share/nginx/html 
+#cd /usr/share/nginx/html
 unzip /tmp/frontend.zip /usr/share/nginx/html
 
-cp '$(conf_file_location)/files/roboshop.conf' /etc/nginx/default.d/roboshop.conf
+cp '${conf_file_location}/files/roboshop.conf' /etc/nginx/default.d/roboshop.conf
 
 systemctl enable nginx 
 systemctl start nginx
