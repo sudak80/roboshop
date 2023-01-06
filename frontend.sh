133f36a3eba828a0081cd7623be11b8926916d84
Sudarsana K <sudak80@gmail.com>
2023-01-06 06:54:40 +0530

    unzip updated

--- frontend.sh
+++ frontend.sh
@@ -12,7 +12,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp '${conf_file_location}/files/roboshop.conf' /etc/nginx/default.d/roboshop.conf
+cp ${conf_file_location}/files/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl enable nginx 
 systemctl start nginx
